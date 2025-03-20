62c40a7d4f5f087fd8cdb8506ca839e76011c127
dyln-n <dylanmanansala13@gmail.com>
2025-03-20 09:48:36 -0400

    Add migrations

---
+++ database/migrations/2025_03_20_133807_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('order_date')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->decimal('total', 10, 2);
+            $table->enum('status', ['pending', 'shipped', 'delivered', 'canceled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ordersv');
+    }
+};
